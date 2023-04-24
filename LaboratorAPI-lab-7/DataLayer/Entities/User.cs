349b76cbaef32cb53aa6c0cc96cc6fe218118b26
ioanaposea <yoa.pos02@gmail.com>
2023-04-24 19:38:04 +0300

    Added migration

--- LaboratorAPI-lab-7/DataLayer/Entities/User.cs
+++ LaboratorAPI-lab-7/DataLayer/Entities/User.cs
@@ -8,5 +8,11 @@ namespace DataLayer.Entities
 {
     public class User : BaseEntity
     {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+
     }
 }
