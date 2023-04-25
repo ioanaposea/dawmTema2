1688658a797dde9a3633142c8309f7443724e89c
ioanaposea <yoa.pos02@gmail.com>
2023-04-25 12:18:28 +0300

    Added authorization service and dtos for login and register

---
+++ LaboratorAPI-lab-7/Core/Dtos/RegisterDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Dtos
+{
+    public class RegisterDto
+    {
+        [Required, MaxLength(100)]
+        public string Firstname { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Lastname { get; set; }
+        
+        [Required, MaxLength(100)]
+        public string Email { get; set; }
+        
+        [Required, MaxLength(100)]
+        public string Password { get; set; }
+        
+        [Required]
+        public int RoleId { get; set; }
+    }
+}
